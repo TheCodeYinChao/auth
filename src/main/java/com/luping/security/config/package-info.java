59fe4595527d6d8c29e50140452a2d471e8d7c71
TheCodeYinChao <292306404@qq.com>
2020-08-18 17:47:58 +0800

    添加sso 示例项目

---
+++ src/main/java/com/luping/security/config/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * description: package-info
+ *
+ * security 核心配置类 都在此
+ * spring-security-oauth-master.rar ： security的oauth2的源码
+ *
+ * sso-merryyou-master.zip  ： 关于使用sso的小示例的项目
+ *
+ * <br>
+ * date: 2020/8/18 17:46 <br>
+ * author: zyc <br>
+ * version: 1.0 <br>
+ */
+package com.luping.security.config;
