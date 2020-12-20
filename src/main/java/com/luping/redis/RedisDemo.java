b2787262d52831b05f0ce701c656230db15de103
zhangyinchao <292306404@qq.com>
2020-12-20 21:41:10 +0800

    redis 相关

--- src/main/java/com/luping/redis/RedisDemo.java
+++ src/main/java/com/luping/redis/RedisDemo.java
@@ -1,16 +1,27 @@
 package com.luping.redis;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
-public class RedisDemo {
+public class RedisDemo implements CommandLineRunner {
 
     @Autowired
     private RedisTemplate redisTemplate;
 
     public  void setStr(String key,String value){
         redisTemplate.opsForValue().set(key,value);
+    }
+
+
+    @Override
+    public void run(String... strings) throws Exception {
+                          redisTemplate.opsForValue().set("a","a");
+        Object a = redisTemplate.opsForValue().get("a");
+
+
+        System.out.println(a);
     }
 }
